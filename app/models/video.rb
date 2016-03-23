c9143ae32080b0fee86bf8583671cb2208fa126a
Anne Jhetson <annepsu@hotmail.com>
2016-03-23 11:04:04 -0700

    add id accessor and todo note

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,11 @@
 class Video < ActiveRecord::Base
   validates :name, presence: true
   validates :file, presence: true
+  # TODO: Validate file size
+  # validates :avatar, file_size: { less_than: 2.gigabytes }
+  # https://github.com/carrierwaveuploader/carrierwave/wiki/How-to%3A-Validate-attachment-file-size
+
+  attr_accessor :id
 
   before_destroy :remove_file
 
